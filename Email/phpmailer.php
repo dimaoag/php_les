1fd4fada5c94f0bf872cb46a646279a432b1aef1
Dmytro Orikhovskyi <dimaoag@gmail.com>
2017-12-21 23:39:59 +0200

    21.12.2017 23:39

--- Email/phpmailer.php
+++ Email/phpmailer.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'vendor/autoload.php';
+require_once '../vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -12,10 +12,10 @@ $mailer->SMTPSecure = "tls";
 $mailer->Host = "smtp.gmail.com";
 $mailer->Port = 587;
 $mailer->Username = "dimaoag@gmail.com";
-$mailer->Password = "******";
+$mailer->Password = "1926odag3";  // Change password!!!!!!!!!!!
 $mailer->SetFrom('dimaoag@gmail.com','Dima');
 $mailer->FromName = "my_site_Dima";
-$mailer->AddAddress("vados271092@gmail.com");
+$mailer->AddAddress("dimaoa@mail.ru");
 $mailer->Subject = "Hello Vados";
 $mailer->Body = "<H3>Hello! Vasya is Apsend Today.</H3>";
 $mailer->IsHTML (true);
